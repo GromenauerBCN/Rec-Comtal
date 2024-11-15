fc35fbd3d9171c456bf530a7e6fb18e75829600b
GromenauerBCN <manelcarrere@gmail.com>
2024-11-15 23:34:33 +0100

    fsadgfa

--- dist/index.672d4772.js
+++ dist/index.672d4772.js
@@ -1,10 +1,10 @@
-// Detectamos el scroll
+
 window.addEventListener('scroll', function() {
     const header = document.getElementById('title-header');
-    const navbar = document.getElementById('navbar');
+    const barra_dalt = document.getElementById('barra_dalt');
     // Añadimos o quitamos la clase 'scrolled' en función del scroll
     if (window.scrollY > 50) document.body.classList.add('scrolled');
     else document.body.classList.remove('scrolled');
 });
 
-//# sourceMappingURL=index.672d4772.js.map
+
