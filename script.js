79e87b82f6b047803b424215a156be887c872c18
GromenauerBCN <manelcarrere@gmail.com>
2024-12-30 00:16:15 +0100

    fsdfadsfd

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// Inicialización de Swiper
 var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    autoplay: {
-      delay: 5000, // Cambia la imagen cada 5 segundos
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: false,
-    },
-  });
-  
+  loop: true,
+  autoplay: {
+    delay: 5000, 
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
