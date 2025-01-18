82ae3cf735335147fff4e4b9086c3307e789fe7c
GromenauerBCN <manelcarrere@gmail.com>
2025-01-18 15:44:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var swiper = new Swiper('.swiper-container', {
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+const swiper = new Swiper('.swiper', {
   loop: true,
   autoplay: {
     delay: 5000, 
